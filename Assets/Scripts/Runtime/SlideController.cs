0249bd7fb7dcd6c9416b362b70f98e1b0513251e
murnana <murnana@users.noreply.github.com>
2021-04-28 09:00:42 +0900

    [WIP] スライドショー作成

---
+++ Assets/Scripts/Runtime/SlideController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ManteraUnityLT20210429
+{
+    /// <summary>
+    /// スライドのコントロールを行います
+    /// </summary>
+    internal sealed class SlideController : MonoBehaviour
+    {
+    }
+}
